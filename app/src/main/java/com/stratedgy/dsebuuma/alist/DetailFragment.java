31d459f3d5d909dacad4704b48f47dbde2ea82bd
Sebuuma Daniel <sedzsoft@gmail.com>
2016-07-18 12:25:12 +0300

    Adds trailer playing feature to the detail view.

--- app/src/main/java/com/stratedgy/dsebuuma/alist/DetailFragment.java
+++ app/src/main/java/com/stratedgy/dsebuuma/alist/DetailFragment.java
@@ -1,17 +1,26 @@
 package com.stratedgy.dsebuuma.alist;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.stratedgy.dsebuuma.alist.model.Movie;
+import com.stratedgy.dsebuuma.alist.model.Youtube;
 import com.stratedgy.dsebuuma.alist.network.Api;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,12 +32,15 @@ public class DetailFragment extends Fragment {
     private static final String LOG_TAG = DetailFragment.class.getSimpleName();
     private String POSTER_URI = "https://image.tmdb.org/t/p/w185";
 
+    ArrayAdapter<Youtube> mTrailerViewAdapter;
+
     private ImageView mPosterView;
     private TextView mTitleView;
     private TextView mOverviewView;
     private TextView mReleaseView;
     private TextView mLengthView;
     private TextView mRatingView;
+    private ListView mTrailerView;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -49,6 +61,7 @@ public class DetailFragment extends Fragment {
         mReleaseView = (TextView) rootView.findViewById(R.id.detail_release_date);
         mLengthView = (TextView) rootView.findViewById(R.id.detail_movie_length);
         mRatingView = (TextView) rootView.findViewById(R.id.detail_movie_rating);
+        mTrailerView = (ListView) rootView.findViewById(R.id.trailer_list_view);
 
         Bundle arguments = getArguments();
         if (arguments != null) {
@@ -80,11 +93,44 @@ public class DetailFragment extends Fragment {
                 mLengthView.setText(movie.getRuntime() + " min");
                 mRatingView.setText(movie.getVoteAverage() + "/10" );
 
+                ArrayList<Youtube> movieTrailers = (ArrayList<Youtube>) movie.getTrailers().getYoutube();
+                mTrailerViewAdapter = new TrailerAdapter(
+                        getContext(),
+                        R.layout.trailer_movie_item,
+                        movieTrailers
+                );
+
                 Picasso.with(getContext())
                         .load(imageUri)
                         .placeholder(R.drawable.placeholder)
                         .error(R.drawable.error)
                         .into(mPosterView);
+
+                mTrailerView.setAdapter(mTrailerViewAdapter);
+
+                mTrailerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Youtube trailer = (Youtube) parent.getItemAtPosition(position);
+
+                        String youtubeUri = "http://www.youtube.com/watch?v=";
+                        String trailerId = trailer.getSource();
+
+                        try {
+                            Intent intent = new Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse("vnd.youtube:" + trailerId)
+                            );
+                            startActivity(intent);
+                        } catch (ActivityNotFoundException ex) {
+                            Intent intent = new Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse(youtubeUri + trailerId)
+                            );
+                            startActivity(intent);
+                        }
+                    }
+                });
             }
 
             @Override
---
+++ app/src/main/java/com/stratedgy/dsebuuma/alist/TrailerAdapter.java
@@ -0,0 +1,50 @@
+package com.stratedgy.dsebuuma.alist;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.stratedgy.dsebuuma.alist.model.Youtube;
+
+import java.util.ArrayList;
+
+public class TrailerAdapter extends ArrayAdapter<Youtube>{
+    private Context context;
+    private int layoutResourceId;
+    private ArrayList data = new ArrayList();
+
+    public TrailerAdapter(Context context, int resource, ArrayList data) {
+        super(context, resource, data);
+
+        this.context = context;
+        this.layoutResourceId = resource;
+        this.data = data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        ViewHolder holder = null;
+
+        if (row == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+            holder = new ViewHolder();
+            holder.trailerPosition = (TextView) row.findViewById(R.id.trailer_position_text_view);
+            row.setTag(holder);
+        } else {
+            holder = (ViewHolder) row.getTag();
+        }
+
+        holder.trailerPosition.setText("Trailer " + (position + 1));
+        return row;
+    }
+
+    static class ViewHolder {
+        TextView trailerPosition;
+    }
+}
